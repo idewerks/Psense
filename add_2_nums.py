48fd36d9aebbff4f8ef13fb3a951cff51797ce05
Kevin Brennan <idewerks@gmail.com>
2023-12-27 23:44:45 -0600

    Add files via upload

---
+++ add_2_nums.py
@@ -0,0 +1,16 @@
+# Python3 program to add two numbers
+# IDEwerks Labview/Python interface test
+
+# Notes- Use minimum of LV2022Q3 version. For older Labview versions, python must be downgraded from 3.9
+# Python MUST use the 32 bit version. This can be checked at startup of the installed interpreter or
+# using sys.maxsize query.
+
+# This test code uses python 3.9.2 32 bit on win10 && LV23
+
+ 
+
+
+def sum_function(num1, num2):
+# Adding two nos
+	return num1 + num2
+
